03cf85addc85fc85de52f93012594257bbfd72e5
Dugiii <dugidemeg@gmail.com>
2025-12-12 00:46:45 -0600

    second example todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,36 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 import { ACTIONS } from './constants'
 import './App.css'
 
 
 function reducer(state, action) {
   switch (action.type) {
-    case ACTIONS.INCREMENT:
-      return {
-        count: state.count + 1
-      }
-  
-    case ACTIONS.DECREMENT:
-      return {
-        count: state.count - 1
-      }
-  
-    default:
-      return state;
+
   }
 }
 
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
 
-  function increment() {
-    dispatch({ type: ACTIONS.INCREMENT });
-  }
-
-  function decrement() {
-    dispatch({ type: ACTIONS.DECREMENT });
-  }
+function handleFormSubmit() {
+  dispatch({
+    type: ACTIONS.ADD_TODO
+  })
+  setName('');
+}
 
   return (
     <div>
-
-      <button className="decrement" onClick={decrement}>
-        –
-      </button>
-      <span className="count">{state.count}</span>
-      <button className="increment" onClick={increment}>
-        +
-      </button>
+      <form onSubmit={handleFormSubmit}>
+        <input 
+          type="text" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          id='todo-input'
+        />
+      </form>
 
     </div>
   )
